7ad9d718529516b33faefdf8f15001ac59a0876b
MistrzuJoTT <jamana113@gmail.com>
2017-11-15 21:08:19 +0100

    add project

---
+++ docs/script.js
@@ -0,0 +1,33 @@
+var colors = [
+"rgb(255, 0, 0)",
+"rgb(255, 255, 0)",
+"rgb(0, 255, 0)",
+"rgb(0, 255, 255)",
+"rgb(0, 0, 255)",
+"rgb(255, 0, 255)"
+]
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+var massageDisplay = document.querySelector("#massege");
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+	//add initial colors to squares
+	squares[i].style.background = colors[i];
+
+	//add click listeners to squares
+	squares[i].addEventListener("click", function(){
+		//grab color of clicked square
+		var clickedColor = this.style.background;
+		//compare color to pickedColor
+		if(clickedColor === pickedColor){
+			alert("Correct!");
+		}else {
+			this.style.background = "#232323";
+			massageDisplay.textContent = "Try Again"
+		}
+	});
+
+}
